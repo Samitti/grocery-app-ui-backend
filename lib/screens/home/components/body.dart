75c86bed01c45b245972b9683a49bcac60a1f216
Hammad Ali <hammadali9064@gmail.com>
2023-03-01 22:30:36 +0500

    Home Screen -- UI -- List View Building Block

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:grocery/constants/dimension.dart';
 import 'package:grocery/screens/home/components/on_sale.dart';
 import 'package:grocery/screens/home/components/swiper_images.dart';
+import 'package:grocery/widgets/text_widget.dart';
 
 class BodyHomeScreen extends StatelessWidget {
   const BodyHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    final AppDimensions dimensions = AppDimensions(context);
+    return Column(
       children: [
-        SwiperImages(),
-        OnSaleWidget(),
+        const SwiperImages(),
+        TextButton(onPressed: (){},
+         child: TextWidget(text: 'View All', color: Colors.blue, textSize: dimensions.getScreenW(20), maxLines: 1,)
+         ,),
+        SizedBox(
+          height: dimensions.getScreenH(200),
+          child: ListView.builder(
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context , index){
+            return const OnSaleWidget();
+          }),
+        )
       ],
     );
   }
--- lib/screens/home/components/on_sale.dart
+++ lib/screens/home/components/on_sale.dart
@@ -12,62 +12,63 @@ class OnSaleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color color = Utils(context).color;
     final AppDimensions dimensions = AppDimensions(context);
-    return InkWell(
-      borderRadius: BorderRadius.circular(12),
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor.withOpacity(0.9),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        padding: EdgeInsets.all(dimensions.getScreenW(15)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(
-                  'https://i.ibb.co/F0s3FHQ/Apricots.png',
-                  height: dimensions.getScreenH(100),
-                  fit: BoxFit.fill,
-                ),
-                Column(
-                  children: [
-                    TextWidget(color: color, text: '1 KG', textSize: dimensions.getScreenW(20), isTitle: true,),
-                    SizedBox(
-                      height: dimensions.getScreenH(6),
-                    ),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {},
-                          child: Icon(
-                            IconlyLight.bag2,
-                            color: color,
-                            size: dimensions.getScreenW(22),
+    return Padding(
+      padding: EdgeInsets.all(dimensions.getScreenW(12)),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor.withOpacity(0.9),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.all(dimensions.getScreenW(15)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    'https://i.ibb.co/F0s3FHQ/Apricots.png',
+                    height: dimensions.getScreenH(90),
+                    fit: BoxFit.fill,
+                  ),
+                  Column(
+                    children: [
+                      TextWidget(color: color, text: '1 KG', textSize: dimensions.getScreenW(20), isTitle: true,),
+                      SizedBox(
+                        height: dimensions.getScreenH(5),
+                      ),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Icon(
+                              IconlyLight.bag2,
+                              color: color,
+                              size: dimensions.getScreenW(22),
+                            ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Icon(
-                            IconlyLight.heart,
-                            color: color,
-                            size: dimensions.getScreenW(22),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Icon(
+                              IconlyLight.heart,
+                              color: color,
+                              size: dimensions.getScreenW(22),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const PriceWidget(),
-            SizedBox(height: dimensions.getScreenH(5),),
-            TextWidget(text: 'Title', color: color, textSize: dimensions.getScreenW(20), isTitle: true,),
-            SizedBox(height: dimensions.getScreenH(5),),
-          ],
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              const PriceWidget(),
+              SizedBox(height: dimensions.getScreenH(5),),
+              TextWidget(text: 'Title', color: color, textSize: dimensions.getScreenW(20), isTitle: true,),
+            ],
+          ),
         ),
       ),
     );

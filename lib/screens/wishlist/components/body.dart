c92daf4a6b4b68121241096ed39578e43898bf12
Hammad Ali <hammadali9064@gmail.com>
2023-03-04 15:40:38 +0500

    WishList Screen -- Grid -- OverFlow Handled -- UI Completed

--- lib/screens/wishlist/components/body.dart
+++ lib/screens/wishlist/components/body.dart
@@ -1,72 +1,19 @@
-import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_iconly/flutter_iconly.dart';
-import 'package:grocery/constants/common_functions.dart';
-import 'package:grocery/constants/dimension.dart';
-import 'package:grocery/constants/utils.dart';
-import 'package:grocery/screens/details/details_screen.dart';
-import 'package:grocery/widgets/heart_widget.dart';
-import 'package:grocery/widgets/text_widget.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:grocery/screens/wishlist/components/product_container.dart';
 
 class BodyWishListScreen extends StatelessWidget {
   const BodyWishListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final AppDimensions dimensions = AppDimensions(context);
-    final Color color = Utils(context).color;
-    return InkWell(
-      onTap: () {
-        CommonFunction.navigateToPage(
-            context: context, routeName: DetailsScreen.routeName);
-      },
-      child: Container(
-        height: dimensions.getScreenH(150),
-        decoration: BoxDecoration(
-            color: Theme.of(context).cardColor.withOpacity(0.5),
-            border: Border.all(
-              color: color,
-              width: 0.5,
-            ),
-            borderRadius: BorderRadius.circular(dimensions.getScreenW(10))),
-        child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: dimensions.getScreenW(10)),
-              width: dimensions.getScreenW(100),
-              height: dimensions.getScreenH(100),
-              child: FancyShimmerImage(
-                imageUrl: 'https://i.ibb.co/F0s3FHQ/Apricots.png',
-                boxFit: BoxFit.fill,
-              ),
-            ),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        IconlyLight.bag2,
-                        color: color,
-                        size: dimensions.getScreenW(25),
-                      ),
-                    ),
-                    HeartWidget(color: color, size: dimensions.getScreenW(25))
-                  ],
-                ),
-                Flexible(
-                  child: TextWidget(text: 'Title', color: color, textSize: dimensions.getScreenW(20), isTitle: true, maxLines: 2,),
-                ),
-                SizedBox(
-                  height: dimensions.getScreenH(10),
-                ),
-                TextWidget(text: '\$2.56', color: color, textSize: dimensions.getScreenW(18), maxLines: 1,),
-              ],
-            )
-          ],
-        ),
-      ),
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      itemBuilder: ((context, index) {
+        return const ProductContainer();
+      }),
     );
   }
 }
---
+++ lib/screens/wishlist/components/product_container.dart
@@ -0,0 +1,89 @@
+
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery/constants/common_functions.dart';
+import 'package:grocery/constants/dimension.dart';
+import 'package:grocery/constants/utils.dart';
+import 'package:grocery/screens/details/details_screen.dart';
+import 'package:grocery/widgets/heart_widget.dart';
+import 'package:grocery/widgets/text_widget.dart';
+
+class ProductContainer extends StatelessWidget {
+  const ProductContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final AppDimensions dimensions = AppDimensions(context);
+    final Color color = Utils(context).color;
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: dimensions.getScreenW(8), vertical: dimensions.getScreenH(10)),
+      child: InkWell(
+        onTap: () {
+          CommonFunction.navigateToPage(
+              context: context, routeName: DetailsScreen.routeName);
+        },
+        child: Container(
+          height: dimensions.getScreenH(130),
+          decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+              border: Border.all(
+                color: color,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(dimensions.getScreenW(10))),
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: dimensions.getScreenW(10)),
+                width: dimensions.getScreenW(90),
+                height: dimensions.getScreenH(90),
+                child: FancyShimmerImage(
+                  imageUrl: 'https://i.ibb.co/F0s3FHQ/Apricots.png',
+                  boxFit: BoxFit.fill,
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          IconlyLight.bag2,
+                          color: color,
+                          size: dimensions.getScreenW(25),
+                        ),
+                      ),
+                      HeartWidget(color: color, size: dimensions.getScreenW(25))
+                    ],
+                  ),
+                  Flexible(
+                    child: TextWidget(
+                      text: 'Title',
+                      color: color,
+                      textSize: dimensions.getScreenW(20),
+                      isTitle: true,
+                      maxLines: 2,
+                    ),
+                  ),
+                  SizedBox(
+                    height: dimensions.getScreenH(10),
+                  ),
+                  TextWidget(
+                    text: '\$2.56',
+                    color: color,
+                    textSize: dimensions.getScreenW(18),
+                    maxLines: 1,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
